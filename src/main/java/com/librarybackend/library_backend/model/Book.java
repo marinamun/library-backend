269cac00703452c722ce83fbff59882e58d41fe6
Marina Muñoz <marinacerejidoo@gmail.com>
2024-10-20 15:56:54 +0200

    Book entity for our BOOKS table in mysql

---
+++ src/main/java/com/librarybackend/library_backend/model/Book.java
@@ -0,0 +1,74 @@
+package main.java.com.librarybackend.library_backend.model;
+
+
+@Entity
+@Table(name = "BOOKS")
+public class Book {
+
+    @Id  // Marks this field as the primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Automatically generates the id
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "published_date")
+    private String publishedDate;
+
+    @Column(name = "is_borrowed")
+    private boolean isBorrowed;
+
+    // CONSTRUCTORS
+    public Book() {}
+
+    public Book(String title, String author, String publishedDate, boolean isBorrowed) {
+        this.title = title;
+        this.author = author;
+        this.publishedDate = publishedDate;
+        this.isBorrowed = isBorrowed;
+    }
+
+    // GETTERS AND SETTERS
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {  // Fixed the typo here
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        isBorrowed = borrowed;
+    }
+}
